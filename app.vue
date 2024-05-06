eae710eb7a1e5617c4ff11f22bc528e30b7301d8
afujiu <akitof11@gmail.com>
2024-05-06 21:26:57 +0900

    camera

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
-  <a-scene vr-mode-ui='enabled: false' arjs='sourceType: webcam; videoTexture: true; debugUIEnabled: false' renderer='antialias: true; alpha: true'>
-      <a-camera gps-new-camera='gpsMinDistance: 5'></a-camera>
-      <a-entity material='color: red' geometry='primitive: box' gps-new-entity-place="latitude: <add-your-latitude>; longitude: <add-your-longitude>" scale="10 10 10"></a-entity>
-  </a-scene>
+    <a-scene embedded arjs="sourceType: webcam;">
+        <!-- マーカー -->
+        <a-marker type="geo" latitude="YOUR_LATITUDE" longitude="YOUR_LONGITUDE">
+            <!-- ここにARオブジェクトを配置 -->
+            <a-box position="0 0.5 0" material="color: red;"></a-box>
+        </a-marker>
+        <!-- カメラとライト -->
+        <a-entity camera></a-entity>
+        <a-light type="ambient" intensity="1"></a-light>
+    </a-scene>
 </template>
 <style>
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,7 @@ export default defineNuxtConfig({
       ],
       script: [
         { src: 'https://aframe.io/releases/1.5.0/aframe.min.js' },
-        { src: 'https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar-threex-location-only.js' },
-        { src: 'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js' },
+        { src: 'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js' },
       ]
     }
   },
