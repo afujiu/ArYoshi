73b8d473e28e8e5db7c6aaf534fd6187b580447d
afujiu <akitof11@gmail.com>
2024-05-06 21:39:49 +0900

    camera

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
     <a-scene embedded arjs="sourceType: webcam;">
         <!-- マーカー -->
-        <a-marker type="geo" latitude="YOUR_LATITUDE" longitude="YOUR_LONGITUDE">
+        <a-marker type="geo" latitude="30" longitude="139">
             <!-- ここにARオブジェクトを配置 -->
             <a-box position="0 0.5 0" material="color: red;"></a-box>
         </a-marker>
@@ -10,5 +10,27 @@
         <a-light type="ambient" intensity="1"></a-light>
     </a-scene>
 </template>
+<script setup lang="ts">
+    const name = ref('');
+    const getLocation=()=>{
+      // ブラウザがGeolocation APIをサポートしているか確認
+      if (navigator.geolocation) {
+          // 位置情報を取得
+          navigator.geolocation.getCurrentPosition((position)=>{
+      // 緯度と経度を取得
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      // 緯度と経度を表示
+      console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+      alert("Latitude: " + latitude + ", Longitude: " + longitude);
+      // ここで取得した緯度と経度を使用して他の処理を行うことができます
+    }, showError);
+      } else {
+          // ブラウザがGeolocation APIをサポートしていない場合のエラーメッセージ
+          alert("Geolocation is not supported by this browser.");
+      }
+    };
+    getLocation()
+</script>
 <style>
 </style>
