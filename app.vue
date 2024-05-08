54510d1abad24f239d14b7a98c3163e90b162faa
afujiu <akitof11@gmail.com>
2024-05-08 21:30:25 +0900

    camera

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
     <a-scene vr-mode-ui="enabled: false" arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-box material="color: red" gps-entity-place="latitude: 35.5686729281061; longitude: 139.74137360557813;" scale="5.5 5.8 5.5"></a-box>
+        <a-box material="color: blue" gps-entity-place="latitude: 35.571163656296285; longitude: 139.74361030574423;" scale="5.5 5.8 5.5"></a-box>
     <a-camera gps-camera rotation-reader> </a-camera>
 </a-scene>
 </template>
