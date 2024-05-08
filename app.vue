a5dbfd72fe953aaf69f3ca2e9e536d3d26ac9a7a
afujiu <akitof11@gmail.com>
2024-05-08 21:07:28 +0900

    camera

--- app.vue
+++ app.vue
@@ -7,26 +7,6 @@
 <script setup lang="ts">
     const lat = ref(0);
     const lon = ref(0);
-    const getLocation=()=>{
-      // ブラウザがGeolocation APIをサポートしているか確認
-      if (navigator.geolocation) {
-          // 位置情報を取得
-          navigator.geolocation.getCurrentPosition((position)=>{
-      // 緯度と経度を取得
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      // 緯度と経度を表示
-      console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-      lat.value = latitude;
-      lon.value =longitude;
-      // ここで取得した緯度と経度を使用して他の処理を行うことができます
-    }, showError);
-      } else {
-          // ブラウザがGeolocation APIをサポートしていない場合のエラーメッセージ
-          alert("Geolocation is not supported by this browser.");
-      }
-    };
-    getLocation()
 </script>
 <style>
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,7 @@ export default defineNuxtConfig({
         { rel: "manifest", href: "/manifest.webmanifest" },
       ],
       script: [
-        { src: 'https://aframe.io/releases/1.5.0/aframe.min.js' },
-        { src: 'https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js' },
+        { src: 'https://aframe.io/releases/1.0.4/aframe.min.js' },
         { src: 'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js' },
       ]
     }
